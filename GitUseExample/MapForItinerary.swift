d820089a0c623d29e994dcae0d2a7aae2282b2a5
wlalzptxm <wlalzptx11@gmail.comm>
2018-08-21 11:57:20 +0900

    Add new map-for-itinerary feature

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Seunghoon Jeon on 21/08/2018.
+//  Copyright © 2018 Seunghoon Jeon. All rights reserved.
+//
+
+import Foundation
+
+class MapForltinerary : NSObject {
+    
+}
