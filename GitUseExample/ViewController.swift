b02f4df4403b77433e2166762cf60d57cc96f39f
wlalzptxm <wlalzptx11@gmail.comm>
2018-08-21 12:20:58 +0900

    Changed the button message

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -21,11 +21,8 @@ class ViewController: UIViewController {
     }
 
     @IBAction func buttonClicked(_ sender: UIButton) {
-        print("This is a Git tutorial")
+        print("You finished!")
     }
     
-    
-    
-
 }
 
